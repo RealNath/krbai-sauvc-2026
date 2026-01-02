59bc4fa97deb6b2eaac2b84f114fb0e3e8d49fb6
RealNath <87565567+RealNath@users.noreply.github.com>
2026-01-02 14:31:02 +0700

    chore: added hard-coded movement (going forward)

--- ros2_ws/src/eggplant/eggplant/foobar.py
+++ ros2_ws/src/eggplant/eggplant/foobar.py
@@ -1,6 +1,28 @@
-def main():
-    print('Hi from eggplant.')
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
 
+class MovementController(Node):
+    def __init__(self):
+        super().__init__('movement_controller')
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        timer_period = 0.1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.get_logger().info('Movement Controller Node has been started.')
+
+    def timer_callback(self):
+        msg = Twist()
+        # Move forward
+        msg.linear.x = 0.5
+        
+        self.publisher_.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    movement_controller = MovementController()
+    rclpy.spin(movement_controller)
+    movement_controller.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
