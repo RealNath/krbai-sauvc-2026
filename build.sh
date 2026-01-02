71f69e71792a48e5a97f59db8292c91537685afd
FieryBanana101 <analisanis4850@gmail.com>
2026-01-02 21:20:20 +0700

    fix: incorrect build.sh file

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-source /opt/ros/humble/setup.bash
-cd krbai-sauvc-2026/ros2_ws
-colcon build
-source install/setup.bash
-ros2 launch eggplant_bringup eggplant.launch.py port:=/dev/ttyACM0 mission:=mission.yaml & 
-exec bash
+sudo docker build --tag "ros-humble-core-dev" .
