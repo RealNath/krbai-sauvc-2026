7f3fb3178f5b2aba4e54143facdb691a3375b33a
FieryBanana101 <analisanis4850@gmail.com>
2026-01-02 21:26:53 +0700

    fix: forgot to add mission.yaml to shell script to launch eggplant

--- raspi-entrypoint.sh
+++ raspi-entrypoint.sh
@@ -3,5 +3,5 @@ source /opt/ros/humble/setup.bash
 cd krbai-sauvc-2026/ros2_ws
 colcon build
 source install/setup.bash
-ros2 launch eggplant_controller eggplant_controller.launch.py port:=/dev/ttyACM0 &
+ros2 launch eggplant_controller eggplant_controller.launch.py port:=/dev/ttyACM0 mission:=mission.yaml &
 exec bash
